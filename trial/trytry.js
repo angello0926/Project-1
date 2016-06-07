97108f9e77fb9c9e0b15c4525139b434ccfd8db0
Angel Lo <angelyukyu@gmail.com>
2016-06-07 09:28:02 +0800

    update

--- trial/trytry.js
+++ trial/trytry.js
@@ -9,7 +9,6 @@ tetris.currentcoor = [ {row:'',col:''},
                        {row:'',col:''} ];
 
 tetris.currshape='O';
-
 tetris.storefield=[];
 
 function grid(){
@@ -19,8 +18,6 @@ function grid(){
          $('[data-row='+i+']').append('<td data-col='+j+'></td>'); //define column
       };
     };
-
-
 };
 
 grid();
@@ -28,77 +25,86 @@ grid();
 function translateshape(currshape,origin){
   switch(currshape){
     case 'O':
-
     return [ {row:origin.row,col:origin.col},
              {row:origin.row,col:origin.col+1},
              {row:origin.row+1,col:origin.col},
              {row:origin.row+1,col:origin.col+1}];
     break;
-
-
   };
-
-
 };
 
 function fillshape(currentcoor,color){
     for (var i=0;i<currentcoor.length;i++){
       $('[data-row='+currentcoor[i].row+']').find('[data-col='+currentcoor[i].col+']').css('background',color);
     };
-
-
 };
 
 tetris.currentcoor=translateshape(tetris.currshape,tetris.origin);
 console.log(tetris.currentcoor);
 fillshape(tetris.currentcoor,'#EEE657');
 
-
-
 function down(){
   var block= false;
-   fillshape(tetris.currentcoor,'');
-   tetris.origin.row++;
+  fillshape(tetris.currentcoor,'');
+  tetris.origin.row++;
+  tetris.currentcoor=translateshape(tetris.currshape,tetris.origin);
+  for (i=0;i<tetris.currentcoor.length;i++){
+      if (tetris.currentcoor[i].row>18||detection(tetris.currentcoor)===1){
+      block=true;
+    }
+  };
 
-   tetris.currentcoor=translateshape(tetris.currshape,tetris.origin);
-   for (i=0;i<tetris.currentcoor.length;i++){
-      if (tetris.currentcoor[i].row>18){
-        block=true;
-      }
-   };
-   if (block===true){
-      tetris.origin.row--;
-      tetris.currentcoor=translateshape(tetris.currshape,tetris.origin);
-      storeshape();
-
+  if (block===true){
+    storeshape();
+    newshape();
+    fillshape(tetris.currentcoor,'#EEE657');
    }
-   console.log(tetris.currentcoor);
-   fillshape(tetris.currentcoor,'#EEE657');
 
+  console.log(tetris.currentcoor);
+  fillshape(tetris.currentcoor,'#EEE657');
 };
 
 
-function storeshape(currentCoor){ //to check whether they can be stored
-     tetris.storefield.push(tetris.currentcoor);
-    for (var i=0; i<tetris.storefield.length; i++){
-      fillshape(tetris.storefield[i],'#EEE657');
+function storeshape(currentcoor){ //to check whether they can be stored
+  tetris.storefield.push(tetris.currentcoor);
+  for (var i=0; i<tetris.storefield.length; i++){
+    fillshape(tetris.storefield[i],'#EEE657');
+  };
+};
 
+function detection(currentcoor){
+  var samecol=0; // samecol= false
+  var samerow=0; //same row = false
+  currentcoor.forEach(function(square){
+    for (var i=0;i<tetris.storefield.length;i++){
+      for (var j=0;j<tetris.storefield[i].length;j++){
+        if ((tetris.storefield[i][j].row)===(square.row)){
+          amerow=1;
+        }
+
+        if (samerow===1){
+          if ((tetris.storefield[i][j].col)===(square.col)){
+            samecol=1;
+          }
+        }
+      };
     };
+  });
+  return samecol;
+};
 
 
+function newshape(){
+  var origin ={row:0,col:5};
+  tetris.currentcoor=translateshape(tetris.currentshape,origin);
 };
 
 
 $(document).keydown(function(e){
-    if (e.keyCode===40){
-      console.log(e.keyCode)
-      down();
-    }
-
-
+  if (e.keyCode===40){
+    console.log(e.keyCode)
+    down();
+  }
 });
 
-
-
-
-})
+});
